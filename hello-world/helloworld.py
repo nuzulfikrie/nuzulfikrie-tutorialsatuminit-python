5554fb542d6f758865f18ef60de3d4fcfbedb7ea
nuzulfikrie <nuzulfikrie@gmail.com>
2023-07-18 12:45:16 +0800

    added folder hello-world

---
+++ hello-world/helloworld.py
@@ -0,0 +1,8 @@
+print("salam dunia")
+
+# ask for user input
+print("siapa nama anda?")
+# store user input in name var
+name = input()
+# we can concatenate strings with + and concatenate a space with " "
+print("selamat datang" + " " + name)
